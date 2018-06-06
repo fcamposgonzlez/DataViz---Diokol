503219681b453ed17975fb2dabb9d0f1cee27fbf
Fabián Francisco Campos González <fcamposgonzlez@gmail.com>
2018-06-06 16:47:09 -0600

    Rainbowness
    Dibujo simple en Diokol

---
+++ Rainbowness/Rainbowness.lua
@@ -0,0 +1,46 @@
+--Rainbowness
+--Belle
+--https://www.openprocessing.org/sketch/512722
+
+function setup() 
+	
+	size(1200,900);
+	background(0);
+
+end
+
+function draw() 
+	strokeWeight(20)
+	stroke(244, 65, 100)
+	point(50,100);--1
+	strokeWeight(50)
+	stroke(247, 150, 4)
+	point(100,150);--2
+	strokeWeight(80)
+	stroke(247, 238, 4);
+	point(190,200);--3
+	strokeWeight(110)
+	stroke(170, 244, 66);
+	point(320,260);--4
+	strokeWeight(140);
+	stroke(65, 244, 163);
+	point(470,300);--5
+	strokeWeight(200)
+	stroke(65, 73, 244)
+	point(650,350);--middle dot
+	strokeWeight(140)
+	stroke(142, 65, 244)
+	point(830,300);---5
+	strokeWeight(110)
+	stroke(232, 65, 244)
+	point(970,260);---4
+	strokeWeight(80)
+	stroke(244, 65, 169)
+	point(1070,200)
+	strokeWeight(50)
+	stroke(244, 65, 97)
+	point(1130,150)
+	strokeWeight(20)
+	stroke(244, 65, 65)
+	point(1170,100)
+end
