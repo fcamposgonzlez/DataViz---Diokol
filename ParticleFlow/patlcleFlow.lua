6d81a074bd2c5985efb7e2a36de4023fab8993c6
jcpb0902 <jcpb0902@gmail.com>
2018-06-06 20:55:03 -0600

    Particle Flow

---
+++ ParticleFlow/patlcleFlow.lua
@@ -0,0 +1,31 @@
+--particleFLow
+--Adaptación por Jean Carlo Paniagua
+
+circle = 200;
+rot = 0
+col = 0
+freq = 0.000005; 
+cont = 0;
+r = 0
+
+function setup() 
+  size(600,600)
+end
+
+function draw() 
+  background(242);
+  translate(300, 300);
+  rotate(radians(rot));
+
+ ellipseMode(RADIUS);
+
+  for i=1, 500 do
+    circle = 200 + 50*math.sin((os.time()*99999999999999999999999999999999)*freq*i);
+    col=map(circle,150,250,255,60);
+    r=map(circle,150,250,5,2);
+    fill(col,0,74);
+    noStroke();
+    ellipse(circle*math.cos(i), circle*math.sin(i),r,r);    
+    rot=rot+0.00005;
+  end
+end
