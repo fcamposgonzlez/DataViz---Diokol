eb7e90b3d0afc7f97d099015475a4a9857cd6cf7
jcpb0902 <jcpb0902@gmail.com>
2018-06-07 18:08:58 -0600

    Olympic Logo

---
+++ OlympicLogo/OlympicLogo .lua
@@ -0,0 +1,38 @@
+--Olympic Logo 
+--https://www.openprocessing.org/sketch/476799
+--Adaptacion por Jean Carlo Paniagua Bastos
+
+
+function setup() 
+  size(1200, 720)
+  background(255, 255, 255);
+
+end
+
+function draw() 
+  noFill(0);
+  stroke(0, 133, 199);
+  ellipse(470, 200, 200, 200);
+  strokeWeight(20);
+  
+  noFill(0);
+  stroke(0, 0, 0);
+  ellipse(700, 200, 200, 200);
+  strokeWeight(20);
+  
+  noFill(0);
+  stroke(222,24,24);
+  ellipse(930, 200, 200, 200);
+  strokeWeight(20);
+  
+  noFill(0);
+  stroke(255, 255, 0);
+  ellipse(585, 320, 200, 200);
+  strokeWeight(20);
+  
+  noFill(0);
+  stroke(8,123,57);
+  ellipse(815, 320, 200, 200);
+  strokeWeight(20);
+
+end
